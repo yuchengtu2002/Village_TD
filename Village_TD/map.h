665d60aa8bdc635c582bcc730a1df7d0cf84eb24
Yucheng Tu <johnsontu2002@gmail.com>
2024-07-17 01:19:47 -0400

    add header file for tiles

---
+++ Village_TD/map.h
@@ -0,0 +1,20 @@
+#ifndef _MAP_H_
+#define _MAP_H_
+
+#include "tile.h"
+
+class Map 
+{
+public:
+		Map() = default;
+		~Map() = default;
+
+private:
+	TileMap tile_map;
+
+
+
+};
+
+
+#endif // !_MAP_H_
---
+++ Village_TD/tile.h
@@ -0,0 +1,30 @@
+#ifndef _TILE_H_
+#define _TILE_H_
+
+#include <vector>
+
+#define TILE_SIZE 48
+
+struct Tile {
+
+	enum class Direction {
+		None = 0,
+		Up = 1,
+		Down = 2,
+		Left = 3,
+		Right = 4
+	};
+
+	int terrian = 0;
+	int decoration = -1;
+	int special_flag = -1;
+	bool has_tower = false;
+
+	Direction direction = Direction::None;
+
+};
+
+typedef std::vector<std::vector<Tile>> TileMap;
+
+
+#endif // !_TILE_H_
